b14686d33cc098074f85af5d6ac2edc89dca3ad0
Ben A. Delat <badelat@gmail.com>
2011-02-26 00:32:43 -0500

    Added updating of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,4 +17,17 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+  def edit
+    @project = Project.find(params[:id])
+  end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      redirect_to @project, notice:"Project has been updated."
+    else
+      flash[:alert] = "Project has not been updated."
+      render action:"edit"
+    end
+  end
+
 end
