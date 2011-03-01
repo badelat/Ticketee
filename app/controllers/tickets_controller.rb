2fb6a8962b88e014d0a8df20bc527cf8a5c30f14
Ben A. Delat <badelat@gmail.com>
2011-02-28 23:47:07 -0500

    Implemented deleting tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -28,6 +28,10 @@ class TicketsController < ApplicationController
     end
   end
 
+  def destroy
+    @ticket.destroy
+    redirect_to @project, notice:"Ticket has been deleted."
+  end
 
   private
     def find_project
